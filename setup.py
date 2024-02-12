a3e6cd5a5be506704778a115b1779c6311021f01
Bastien Chappis <chappis.bastien@gmail.com>
2024-02-12 18:10:20 +0100

    cookiecutter setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The project is a template for building data science code repositories',
+    author='bastien chappis',
+    license='MIT',
+)
